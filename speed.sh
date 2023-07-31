19085f9bd963c26f061e73b08c1447a866d5a40d
lz0423 <1248820667@qq.com>
2023-08-01 00:52:03 +0800

    Update speed.sh

--- speed.sh
+++ speed.sh
@@ -2,7 +2,8 @@
 
 num_threads=10  # 设置线程数
 num_loops=99999999999  # 设置总循环次数
-url="https://storage.jd.com/jdmobile/JDMALL-PC2.apk"  # 下载链接
+url="https://13d9d95a733ff7b76f0f17481fa84231.dlied1.cdntips.net/dlied4.myapp.com/myapp/1104466820/cos.release-40109/10040714_com.tencent.tmgp.sgame_a2680838_8.4.1.6_fL2tC9.apk?mkey=lego_ztc&f=00&sche_type=7&cip=42.203.52.30&proto=https&access_type="
+#url="https://storage.jd.com/jdmobile/JDMALL-PC2.apk"  # 下载链接
 
 function stop_processes {
   echo "接收到 SIGINT 信号，正在终止进程..."
