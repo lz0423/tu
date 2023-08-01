f850e28162cfbd5e7cdfc72bde0b719f6e4b1ff0
lz0423 <1248820667@qq.com>
2023-08-01 13:09:38 +0800

    Update speedauto.sh

--- speedauto.sh
+++ speedauto.sh
@@ -5,7 +5,7 @@ num_loops=9999999999  # 设置总循环次数
 url="https://yun.mcloud.139.com/mCloud/mCloud_10.1.1_2310111_000.apk"  # 下载链接
 
 # 引入 input.sh 脚本，获取用户输入流量大小和单位
-source ~ https://ghproxy.com/https://raw.githubusercontent.com/lz0423/tu/main/input.sh
+source ~ ./input.sh
 
 case $unit in
   "mb"|"MB")
